d7534131a4183bcb16cda8c8e912fc720eb7d9cd
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-26 01:34:09 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Принимает число ,возводит его в квадрат и выводит
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+Console.WriteLine(result);
