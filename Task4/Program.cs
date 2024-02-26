53e6b1432bebc624fadd263e7907c8833d12e0b6
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 02:20:35 +0300

    Решил 4 задачу

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает значение чиcла N, а на выходе выдает
+// все числа в промежутке от -N до N
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negNum = num * -1;                  // Или можно int negNum = -num;
+
+while(negNum <= num)
+{
+    Console.Write(negNum);              // Или можно Console.Write(negNum + " ");
+    Console.Write(" ");
+    negNum = negNum + 1;
+}
