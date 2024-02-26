28cf378eb40c282d769055fcfbe194a3be1d0ed3
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 02:08:59 +0300

    Решил 3 задачу

---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа, которая выводит день недели по порядковому номеру
+
+Console.WriteLine("Введите номер дня недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num == 2)                                  //else ииспользуется для остановки проверки последующих значений!
+{
+    Console.WriteLine("Вторник");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Не обнаружено соответствующего дня недели!");
+}
