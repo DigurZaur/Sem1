b0c67743668e72896fed2c25455451544ebba982
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 02:28:51 +0300

    Решил 5 задачу

---
+++ Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает трехзначное число
+// и выводит крайнюю цифру на входе.    857 -> 7
+
+Console.WriteLine("Ввдите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num > 99) && (num < 1000))
+{
+    int a = num % 10;
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine("Число не является трехзначным!");
+}
